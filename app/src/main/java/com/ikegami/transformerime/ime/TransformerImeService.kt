2ae80ea27851c2cc6ad3faa863f160599ff40477
IKEGAMI-99 <hato.jp@gmail.com>
2026-08-24 14:15:16 +0900

    fix: use correct InputMethodService package

--- app/src/main/java/com/ikegami/transformerime/ime/TransformerImeService.kt
+++ app/src/main/java/com/ikegami/transformerime/ime/TransformerImeService.kt
@@ -8,7 +8,7 @@ import android.view.Gravity
 import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.EditorInfo
-import android.view.inputmethod.InputMethodService
+import android.inputmethodservice.InputMethodService
 import android.widget.Button
 import android.widget.HorizontalScrollView
 import android.widget.LinearLayout
